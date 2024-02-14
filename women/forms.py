98c85f720a582f026b264601c275eb4aa1fe9e3e
Dimon Kasyanenko <dima@webdima31.ru>
2024-02-14 16:38:29 +0300

    7.4 Валидация полей формы. DONE!

--- women/forms.py
+++ women/forms.py
@@ -1,12 +1,21 @@
 from django import forms
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label='URL')
-    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Описание')
+    title = forms.CharField(max_length=255, min_length=3, label='Заголовок',
+                            widget=forms.TextInput(attrs={'class': 'form-input'}),
+                            error_messages={'required': 'Без заголовка ни как',
+                                            'min_length': 'Минимально должно быть 3 символа'
+                                            })
+    slug = forms.SlugField(max_length=255, label='URL', validators=[
+        MinLengthValidator(5, message=),
+        MaxLengthValidator(255)
+    ])
+    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False,
+                                  label='Описание')
     is_published = forms.BooleanField(required=False, label='Статус', initial=True)
     category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не '
                                                                                                       'выбрана')
