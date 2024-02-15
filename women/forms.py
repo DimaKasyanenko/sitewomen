eab555f90d48f66fd8e0b58c92cfecbc4914fef6
Dimon Kasyanenko <dima@webdima31.ru>
2024-02-15 14:36:58 +0300

    7.5 Формы связанные с моделями. DONE!

--- women/forms.py
+++ women/forms.py
@@ -1,23 +1,36 @@
 from django import forms
-from django.core.validators import MinLengthValidator, MaxLengthValidator
 
-from .models import Category, Husband
+from .models import Category, Husband, Women
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, min_length=3, label='Заголовок',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}),
-                            error_messages={'required': 'Без заголовка ни как',
-                                            'min_length': 'Минимально должно быть 3 символа'
-                                            })
-    slug = forms.SlugField(max_length=255, label='URL', validators=[
-        MinLengthValidator(5),
-        MaxLengthValidator(255)
-    ])
-    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False,
-                                  label='Описание')
-    is_published = forms.BooleanField(required=False, label='Статус', initial=True)
+class AddPostForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не '
                                                                                                       'выбрана')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), label='Муж', empty_label='Не замужем',
                                      required=False)
+
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'description', 'is_published', 'category', 'husband', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'description': forms.Textarea(attrs={'cols': 50,  'rows': 5}),
+            'tags': forms.CheckboxSelectMultiple()
+        }
+# class AddPostForm(forms.Form):
+#     title = forms.CharField(max_length=255, min_length=3, label='Заголовок',
+#                             widget=forms.TextInput(attrs={'class': 'form-input'}),
+#                             error_messages={'required': 'Без заголовка ни как',
+#                                             'min_length': 'Минимально должно быть 3 символа'
+#                                             })
+#     slug = forms.SlugField(max_length=255, label='URL', validators=[
+#         MinLengthValidator(5),
+#         MaxLengthValidator(255)
+#     ])
+#     description = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False,
+#                                   label='Описание')
+#     is_published = forms.BooleanField(required=False, label='Статус', initial=True)
+#     category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не '
+#                                                                                                       'выбрана')
+#     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), label='Муж', empty_label='Не замужем',
+#                                      required=False)
