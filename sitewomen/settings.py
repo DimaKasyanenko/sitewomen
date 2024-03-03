4086db34b9d390a620e359950c8a349bf2423b27
Dimon <dima@webdima31.ru>
2024-03-03 21:49:21 +0300

    9.13 Расширение модели User. Класс AbstractUser, 8 минут

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -142,6 +142,16 @@ AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailAuthBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+AUTH_USER_MODEL = 'users.User'
 
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'webdima31@yandex.ru'
+EMAIL_USE_SSL = True
+EMAIL_HOST_PASSWORD = 'cfkvjrvlrqmknktu'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
