4ab4bd4f3ecb119216a25bc06db7fc1054dd3bf7
Dimon <dima@webdima31.ru>
2024-03-03 14:37:47 +0300

    9.4 Классы LoginView, LogoutView и AuthenticationForm. DONE!

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -116,6 +116,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'users:login'
+# LOGOUT_REDIRECT_URL = 'home'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
