5cd3d03d1979319f24e6de8fe405120ad7134e14
Dimon <dima@webdima31.ru>
2024-03-03 18:56:54 +0300

    9.6 Регистрация пользователей через функции представления. DONE!

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
+    path('register/', register, name='register'),
     path('logout/', logout_user, name='logout'),
 ]
